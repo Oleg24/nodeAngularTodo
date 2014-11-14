ca02c62300153e22b5c12332ba43f41da34619ac
Oleg <o.yanchinskiy@gmail.com>
2014-11-13 23:18:25 -0800

    Complete API path to todos

--- server.js
+++ server.js
@@ -35,5 +35,77 @@ app.use(bodyParser.json({ type: 'application/vnd/api + json' }));
 
 app.use(methodOverride());
 
+// Define the model 
+var toDo = mongoose.model('Todo', {
+	text: String
+})
+
+// Setting up the routes //  
+
+// api 
+// get all todos
+
+app.get('/api/todos', function(req, res){
+	// use mongoose to get all todos in the DB 
+	toDo.find(function(err, todos){
+		// if there is an error retrieving, send the error, nothing after res.send(err) will execute
+		if(err){
+			res.send(err);
+		};
+		res.json(todos) // return all the todos in JSON format
+	});
+});
+
+// create todo and send back all todos after the creation
+app.post('/api/todos', function(req, res){
+
+	// create a todo, information comes from AJAX request in Angular 
+	toDo.create({
+		text : req.body.text,
+		done : false
+	}, function(err, todos){
+		if(err) res.send(err);
+
+		toDo.find(function(err, todos){
+			if(err) res.send(err);
+			res.json(todos);
+		});
+	});
+});
+
+//delete a todo
+app.delete('/api/todos/:todo_id', function(req, res){
+	toDo.remove({
+		_id : req.params.todo_id
+	}, function(err, todo){
+		 	if(err) res.send(err);
+		 	
+
+		 	// get and return all the todos after you delete one
+		 	Todo.find(function(err, todos){
+		 		if(err) res.send(err);
+		 		res.json(todos);
+		 	});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(8080);
 console.log("App listening on port 8080");
+
+
+
+

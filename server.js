4f0c2fe54b78092cd716c518d77bfee16fef0285
Oleg <o.yanchinskiy@gmail.com>
2014-11-14 20:56:07 -0800

    Set up server to send responses to index.html

--- server.js
+++ server.js
@@ -89,6 +89,11 @@ app.delete('/api/todos/:todo_id', function(req, res){
 	});
 });
 
+app.get('*', function(req, res){
+	res.sendfile('./public/index.html');
+	// load the single view file - angular will handle the page changes on the front end
+})
+
 app.listen(8080);
 console.log("App listening on port 8080");
 
